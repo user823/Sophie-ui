78ac6c1b024f7ee6f9bbbd3dedb712bb8e6343e0
user823 <869860837@qq.com>
2024-02-29 14:59:38 +0800

    feat(all-code): base-ui
    BREAKING CHANGE:

---
+++ src/components/Sophie/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SophieGit',
+  data() {
+    return {
+      url: 'https://github.com/user823/Sophie-ui'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
