aa7facb8b9a59326f074fc95528df5b4008f9dbf
user823 <869860837@qq.com>
2024-04-01 17:17:07 +0800

    change index.vue

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -307,36 +307,6 @@
         <el-button @click="cancel">取 消</el-button>
       </div>
     </el-dialog>
-
-    <!-- 用户导入对话框 -->
-    <!-- <el-dialog :title="upload.title" :visible.sync="upload.open" width="400px" append-to-body>
-      <el-upload
-        ref="upload"
-        :limit="1"
-        accept=".xlsx, .xls"
-        :headers="upload.headers"
-        :action="upload.url + '?updateSupport=' + upload.updateSupport"
-        :disabled="upload.isUploading"
-        :on-progress="handleFileUploadProgress"
-        :on-success="handleFileSuccess"
-        :auto-upload="false"
-        drag
-      >
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip text-center" slot="tip">
-          <div class="el-upload__tip" slot="tip">
-            <el-checkbox v-model="upload.updateSupport" /> 是否更新已经存在的用户数据
-          </div>
-          <span>仅允许导入xls、xlsx格式文件。</span>
-          <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;" @click="importTemplate">下载模板</el-link>
-        </div>
-      </el-upload>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitFileForm">确 定</el-button>
-        <el-button @click="upload.open = false">取 消</el-button>
-      </div>
-    </el-dialog> -->
   </div>
 </template>
 
@@ -388,21 +358,6 @@ export default {
         children: "children",
         label: "label"
       },
-      // 用户导入参数
-      // upload: {
-      //   // 是否显示弹出层（用户导入）
-      //   open: false,
-      //   // 弹出层标题（用户导入）
-      //   title: "",
-      //   // 是否禁用上传
-      //   isUploading: false,
-      //   // 是否更新已经存在的用户数据
-      //   updateSupport: 0,
-      //   // 设置上传的请求头部
-      //   headers: { Authorization: "Bearer " + getToken() },
-      //   // 上传的地址
-      //   url: process.env.VUE_APP_BASE_API + "/system/user/importData"
-      // },
       // 查询参数
       queryParams: {
         pageNum: 1,
@@ -579,6 +534,7 @@ export default {
         this.roleOptions = response.roles;
         this.$set(this.form, "postIds", response.postIds);
         this.$set(this.form, "roleIds", response.roleIds);
+        console.log("执行了")
         this.open = true;
         this.title = "修改用户";
         this.form.password = "";
@@ -639,11 +595,6 @@ export default {
         ...this.queryParams
       }, `user_${new Date().getTime()}.xlsx`)
     },
-    /** 导入按钮操作 */
-    // handleImport() {
-    //   this.upload.title = "用户导入";
-    //   this.upload.open = true;
-    // },
     /** 下载模板操作 */
     importTemplate() {
       this.download('system/user/importTemplate', {
@@ -653,14 +604,6 @@ export default {
     handleFileUploadProgress(event, file, fileList) {
       this.upload.isUploading = true;
     },
-    // 文件上传成功处理
-    // handleFileSuccess(response, file, fileList) {
-    //   this.upload.open = false;
-    //   this.upload.isUploading = false;
-    //   this.$refs.upload.clearFiles();
-    //   this.$alert("<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" + response.msg + "</div>", "导入结果", { dangerouslyUseHTMLString: true });
-    //   this.getList();
-    // },
     // 提交上传文件
     submitFileForm() {
       this.$refs.upload.submit();
